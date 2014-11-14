1b0d7e0cb30ed43c6d8ef80e72f4fc0baefc265c
arter97 <qkrwngud825@gmail.com>
2014-11-14 18:43:27 +0900

    msm: mpm-of: move mutex declaration out of the function
    Mutex needs to be declared outside of a function to work properly.
    
    SM-G900F follows the same.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- arch/arm/mach-msm/mpm-of.c
+++ arch/arm/mach-msm/mpm-of.c
@@ -40,6 +40,8 @@
 #include <mach/clk.h>
 #include <mach/rpm-regulator-smd.h>
 
+static DEFINE_MUTEX(enable_xo_mutex);
+
 enum {
 	MSM_MPM_GIC_IRQ_DOMAIN,
 	MSM_MPM_GPIO_IRQ_DOMAIN,
@@ -573,8 +575,6 @@ void msm_mpm_exit_sleep(bool from_idle)
 }
 static void msm_mpm_sys_low_power_modes(bool allow)
 {
-	static DEFINE_MUTEX(enable_xo_mutex);
-
 	mutex_lock(&enable_xo_mutex);
 	if (allow) {
 		if (xo_enabled) {
