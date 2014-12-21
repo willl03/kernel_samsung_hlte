16e2dc1243d252a50c2e7ce62ba8b3291317cd9f
christer12 <christer.bjor@gmail.com>
2014-12-21 07:24:03 +0100

    Revert "rtc: alarm: Proper handling of spinlock" Improper handling ultimately breaks reboot to recovery/download.

--- drivers/rtc/alarm.c
+++ drivers/rtc/alarm.c
@@ -639,8 +639,6 @@ static void alarm_shutdown(struct platform_device *dev)
 	if (!power_on_alarm)
 		goto disable_alarm;
 
-	spin_unlock_irqrestore(&alarm_slock, flags);
-
 	rtc_read_time(alarm_rtc_dev, &rtc_time);
 	getnstimeofday(&wall_time);
 	rtc_tm_to_time(&rtc_time, &rtc_secs);
@@ -667,6 +665,7 @@ static void alarm_shutdown(struct platform_device *dev)
 		pr_alarm(FLOW, "Power-on alarm set to %lu\n",
 				alarm_time);
 
+	spin_unlock_irqrestore(&alarm_slock, flags);
 	return;
 
 disable_alarm:
